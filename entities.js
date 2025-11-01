d9daa365b028ec54c42e5517b0d4da7ca6cce1b8
911smartfix <911smartfix@gmail.com>
2025-11-01 07:50:08 -0400

    Add files via upload

---
+++ entities.js
@@ -0,0 +1,75 @@
+import { base44 } from './base44Client';
+
+
+export const Customer = base44.entities.Customer;
+
+export const Order = base44.entities.Order;
+
+export const Transaction = base44.entities.Transaction;
+
+export const CashRegister = base44.entities.CashRegister;
+
+export const Product = base44.entities.Product;
+
+export const Service = base44.entities.Service;
+
+export const TimeEntry = base44.entities.TimeEntry;
+
+export const CashDrawerMovement = base44.entities.CashDrawerMovement;
+
+export const SystemConfig = base44.entities.SystemConfig;
+
+export const Sale = base44.entities.Sale;
+
+export const DeviceModel = base44.entities.DeviceModel;
+
+export const EmailLog = base44.entities.EmailLog;
+
+export const InventoryMovement = base44.entities.InventoryMovement;
+
+export const AuditLog = base44.entities.AuditLog;
+
+export const WorkOrderConfig = base44.entities.WorkOrderConfig;
+
+export const DeviceCategory = base44.entities.DeviceCategory;
+
+export const Brand = base44.entities.Brand;
+
+export const DeviceSubcategory = base44.entities.DeviceSubcategory;
+
+export const DeviceFamily = base44.entities.DeviceFamily;
+
+export const CatalogImportLog = base44.entities.CatalogImportLog;
+
+export const ModelCompatibility = base44.entities.ModelCompatibility;
+
+export const WorkOrderEvent = base44.entities.WorkOrderEvent;
+
+export const KeyValue = base44.entities.KeyValue;
+
+export const CommunicationQueue = base44.entities.CommunicationQueue;
+
+export const Role = base44.entities.Role;
+
+export const Permission = base44.entities.Permission;
+
+export const RolePermission = base44.entities.RolePermission;
+
+export const UserRole = base44.entities.UserRole;
+
+export const TechnicianProfile = base44.entities.TechnicianProfile;
+
+export const TechnicianMetrics = base44.entities.TechnicianMetrics;
+
+export const Supplier = base44.entities.Supplier;
+
+export const PurchaseOrder = base44.entities.PurchaseOrder;
+
+export const AppSettings = base44.entities.AppSettings;
+
+export const AppEmployee = base44.entities.AppEmployee;
+
+
+
+// auth sdk:
+export const User = base44.auth;
